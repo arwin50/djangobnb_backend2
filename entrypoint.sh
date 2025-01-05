4bd6fadbc8e8dc0b32a2491f6f884eaffbefc967
powtzulist <delasanarwin@gmail.com>
2025-01-05 18:12:59 +0800

    [setup] configurations in settings

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
-if ["$DATABASE" = "postgres"]
-then
-    echo "Check if database is runnig..."
+if [ "$DATABASE" = "postgres" ]; then
+    echo "Checking if the database is running..."
 
-    while ! nc -z $SQL_HOST $SQL_PORT; do
+    while ! nc -z "$SQL_HOST" "$SQL_PORT"; do
         sleep 0.1
     done
 
     echo "The database is up and running ;-D"
-
 fi
 
+# Apply migrations
 python manage.py migrate
 
+# Execute the command passed to the container
 exec "$@"
