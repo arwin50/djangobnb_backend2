b4e2f5640672da9002982fbedfcfc21c5b3a6c77
powtzulist <delasanarwin@gmail.com>
2025-01-09 19:35:35 +0800

    [setup] websockets for messaging

--- djangobnb_backend/settings.py
+++ djangobnb_backend/settings.py
@@ -24,6 +24,12 @@ SITE_ID = 1
 
 WEBSITE_URL = 'http://localhost:8000'
 
+CHANNEL_LAYERS={
+    'default':{
+        'BACKEND': 'channels.layers.InMemoryChannelLayer'
+    }
+}
+
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
@@ -66,6 +72,7 @@ REST_AUTH = {
 # Application definition
 
 INSTALLED_APPS = [
+    'daphne',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -115,6 +122,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'djangobnb_backend.wsgi.application'
+ASGI_APPLICATION = 'djangobnb_backend.asgi.application'
 
 
 # Database
